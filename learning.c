818dca2604fba510444af53bbb03993870eb7900
Nizar <nizarbadriali6@gmail.com>
2026-09-14 16:04:07 -0700

    working with arrays and output

--- learning.c
+++ learning.c
@@ -1,11 +1,17 @@
-#include <stdio.h> // Library needed for printf()
-#include <stdlib.h> // Library needed for rand(), srand()
+#include <stdio.h>
     
 int main() {
-    int i, val;
-    srand(50); // Seed the random number generator with a fixed value for reproducibility
-    for (i = 0; i < 4; i++) {
-        val = rand(); // Generate a (pseudo) random value between 0 and RAND_MAX
-        printf("%d ", val);
+    int i;
+    double grades [5]; // Array to hold 5 grades
+    grades [0] = 85.5; // Assigning values to the array
+    grades [1] = 90.0;
+    grades [2] = 78.5;
+    grades [3] = 92.0;
+    grades [4] = 88.5;
+    for (i = 0; i < 5; i++) {
+        printf("Grade %d: %.1f\n", i, grades[i]); // Print each grade
+        printf("%p\n", &grades[i]); // Print the address of each grade
+        printf("At index: grades[%d]\n", i);   
     }
+    return 0;
 }
