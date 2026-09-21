0b54ca97bfe86b3a84f48fe81901cc1d239ccb77
Nizar <nizarbadriali6@gmail.com>
2026-09-20 18:28:29 -0700

    working with pre and post increments

--- learning.c
+++ learning.c
@@ -2,14 +2,11 @@
 
 int main() {
 
-    int a[3] = {423, 12, -780};
-    int *ptr;
-
-    ptr = &a[0];
-
-    printf("%d \t", *ptr );
-    printf("%d \t", *(ptr+1) ); // ptr goes up 1 memory then derefencers, 
-    printf("%d \t", *(ptr+2) );
+    int x = 5;
+    int y = ++x * 3;
+    
+    printf("Value of y: %d\n", y);
+    printf("Value of x: %d\n", x);
 
     return 0;
 }
