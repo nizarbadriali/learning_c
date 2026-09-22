8239cab70faa2901295c56d4483a5e0fb77f718b
Nizar <nizarbadriali6@gmail.com>
2026-09-21 19:10:00 -0700

    complicating the function

--- learning.c
+++ learning.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
-int* process_array(int *ptr) {
-    *(ptr + 1) = *(ptr + 1) + 5;   // Line 1
-    ptr = ptr + 2;                 // Line 2
-    *ptr = *ptr - 10;              // Line 3
+int* ModifyValues(int *ptr) {
+    *(ptr - 1) = *(ptr - 1) + 2;   // Line 1
+    *(ptr + 1) = *(ptr + 1) * 3;   // Line 2
+    ptr = ptr + 1;                 // Line 3
     return ptr;                    // Line 4
 }
 
 int main() {
-    int arr[5] = {10, 20, 30, 40, 50};
+    int arr[5] = {2, 4, 6, 8, 10};
     int *p;
 
-    p = process_array(arr + 1);
+    p = ModifyValues(arr + 2);
 
     printf("%d\n", arr[1]);
     printf("%d\n", arr[3]);
     printf("%d\n", p[0]);
-    printf("%d\n", p[-1]);
+    printf("%d\n", p[-2]);
 
     return 0;
-}
+}x
