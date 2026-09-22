01c60c2d5b67f76986d0b73152e29f275d3e3828
Nizar <nizarbadriali6@gmail.com>
2026-09-21 19:05:43 -0700

    working more

--- learning.c
+++ learning.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
-int* modify_and_shift(int *ptr) {
-    *ptr = *ptr * 2;   // Step 1
-    ptr = ptr - 1;     // Step 2
-    return ptr;        // Step 3
+int* process_array(int *ptr) {
+    *(ptr + 1) = *(ptr + 1) + 5;   // Line 1
+    ptr = ptr + 2;                 // Line 2
+    *ptr = *ptr - 10;              // Line 3
+    return ptr;                    // Line 4
 }
 
 int main() {
     int arr[5] = {10, 20, 30, 40, 50};
     int *p;
 
-    p = modify_and_shift(arr + 3);
+    p = process_array(arr + 1);
 
+    printf("%d\n", arr[1]);
     printf("%d\n", arr[3]);
     printf("%d\n", p[0]);
-    printf("%d\n", p[1]);
+    printf("%d\n", p[-1]);
 
     return 0;
 }
